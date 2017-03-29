95da673d2a6e2d643eae175af97b39692eb65ab7
Marc Bormeth <marc.bormeth@htwg-konstanz.de>
2017-03-29 13:44:10 +0200

    build succeeded

--- v1/switch.c
+++ v1/switch.c
@@ -1,6 +1,6 @@
 //
 //  switch.c
-//  
+//
 //
 //  Created by Marc Bormeth on 29.03.17.
 //
@@ -9,36 +9,97 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <time.h>
+#include <errno.h>
+#include <limits.h>
 
-const int PATHSIZE = 15;
+const int PATHSIZE = 21;
 const char* PATH = "/sys/class/gpio";
+const int PIN = 17;
+const int SLEEPTIME = 100000; // 1/10 second
 
-int readGPIO(int pin, int mode) {
-    int value = -1;
+
+
+
+int validateInt(char* str){
+    char* ep;
+    long val;
     
+    //Snippet from the manpage
+    errno = 0;
+    val = strtoul(str, &ep, 10);
+    
+    if ((errno == ERANGE && (val == LONG_MAX || val == LONG_MIN))
+        || (errno != 0 && val == -1)) {
+        perror("Strtol failed ");
+        exit(EXIT_FAILURE);
+    }
+    
+    //ep == str --> does not start with a num
+    //*ep != '\0' --> only a part of the string is a num
+    //int range checks
+    if (ep == str || *ep != '\0' || val > INT_MAX || val < INT_MIN) {
+        fprintf(stderr, "%s cannot be parsed to a int\n", str);
+        exit(EXIT_FAILURE);
+    }
+    
+    return (int)val;
+}
+
+int readGPIO(int pin) {
+    int value = -1; //default ret
+    
+    
+    //preparing for open
     int sizeConcat = PATHSIZE + 1;
     
     if (pin >= 10)
         sizeConcat++;
     
-    char concatPath = [sizeConcat];
-    sprintf(concatPath, "%s%d", PATH, pin);
+    char* concatPath = (char*) malloc(sizeConcat + 1); //nullbyte
+    sprintf(concatPath, "%s%d%s", PATH, pin, "/value");
     
-    if ((int gpio = open(concatPath, mode)) != -1){
-        value = read(gpio);
-        close(concatPath)
+    //opening and reading Pin
+    FILE *gpio;
+    char buffer[10];
+    
+    gpio = fopen(concatPath, O_RDONLY);
+    if (gpio != NULL){
+        fread(buffer, 10, 1, gpio);
+        fclose(gpio);
+        value = validateInt(buffer);
     }
     
     return value;
 }
 
 
+
 int main() {
+    int isRunning = 1;
+    int oldValue = 1; //default value on rasp pi
+    int numberOfPushes = 0;
+    int val;
     
-    
+    while (isRunning) {
+        if ((val = readGPIO(PIN)) > -1) {
+            
+            if (val != oldValue ) {
+                oldValue = val;
+                if (val == 0) {
+                    numberOfPushes++;
+                    printf("Pushes: %d", numberOfPushes);
+                }
+            }
+            
+        }
+        usleep(SLEEPTIME);
+        
+    }
     
     
     return EXIT_SUCCESS;
