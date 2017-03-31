b1d0fba944ba44ffb33ffbbb53dbca9feda0af97
Henry <blauherr@yahoo.de>
2017-03-31 12:39:11 +0200

    refactor names, verbose flag

--- v1/led5.c
+++ v1/led5.c
@@ -38,33 +38,36 @@ const int BLINK_HERTZ = 5;
 volatile int enableBlinking = 1;
 volatile int killswitch = 1;
 
+int verbose = 0;
+
 void enforceMalloc(void* ptr) {
-    if (ptr == NULL) {
-        //If malloc failes, perror failes also. A direct call works.
-        char * out = "Malloc failed!\n";
-        write(2, out, sizeof("Malloc failed!\n"));
-        //perror("Malloc failed!");
-        exit(EXIT_FAILURE);
-    }
+	if (ptr == NULL) {
+		//If malloc failes, perror failes also. A direct call works.
+		char * out = "Malloc failed!\n";
+		write(2, out, sizeof("Malloc failed!\n"));
+		//perror("Malloc failed!");
+		exit(EXIT_FAILURE);
+	}
 }
 
 
 char* getGPIOPath(char* pin, const char* suffix) {
 	 //preparing for open
-    int sizeConcat = strlen(PATH) + strlen(pin) + strlen(suffix);
-    
-    char* concatPath = (char*) malloc(sizeConcat + 1); //nullbyte
-    enforceMalloc(concatPath);
-    sprintf(concatPath, "%s%s%s", PATH, pin, suffix);
-    
-    return concatPath;
+	int sizeConcat = strlen(PATH) + strlen(pin) + strlen(suffix);
+	
+	char* concatPath = (char*) malloc(sizeConcat + 1); //nullbyte
+	enforceMalloc(concatPath);
+	sprintf(concatPath, "%s%s%s", PATH, pin, suffix);
+	
+	return concatPath;
 }
 
 void writeSafe(char* path, char* value){
-	printf("writeSafe path %s value %s\n", path, value);
+	if (verbose) printf("writeSafe path %s value %s\n", path, value);
+	
 	FILE *gpio;
-    gpio = fopen(path, "w");
-    if (gpio != NULL){
+	gpio = fopen(path, "w");
+	if (gpio != NULL){
 		if (fwrite(value, sizeof(char), strlen(value), gpio) != strlen(value)){
 			perror("fwrite failed");
 			exit(EXIT_FAILURE);
@@ -78,30 +81,30 @@ void writeSafe(char* path, char* value){
 			perror("writeSafe: fclose failed");
 			exit(EXIT_FAILURE);
 		}
-    } else {
+	} else {
 		perror("writeSafe: fopen failed");
 		exit(EXIT_FAILURE);
-    }
+	}
 }
 
 
 int readGPIO(char* pin) {
-    int value = -1; //default ret
-    int readCount = 0;
-    FILE *gpio;
-    char buffer[BUFFER_SIZE];
+	int value = -1; //default ret
+	int readCount = 0;
+	FILE *gpio;
+	char buffer[BUFFER_SIZE];
 	
 	char* path = getGPIOPath(pin, PATH_SUFFIX_VALUE);
-    
-    gpio = fopen(path, "r");
-    if (gpio != NULL){
-        if ((readCount = fread(buffer, 1, BUFFER_SIZE, gpio)) != 2) {
+	
+	gpio = fopen(path, "r");
+	if (gpio != NULL){
+		if ((readCount = fread(buffer, 1, BUFFER_SIZE, gpio)) != 2) {
 			perror("fread failed");
 			printf("cnt %d\n", readCount);
 			exit(EXIT_FAILURE);
 		}
 		
-        value = buffer[0] - '0'; 
+		value = buffer[0] - '0'; 
 		
 		//value validty check
 		if (value != 0 && value != 1){
@@ -113,29 +116,29 @@ int readGPIO(char* pin) {
 			perror("fclose failed");
 			exit(EXIT_FAILURE);
 		}
-    } else {
+	} else {
 		perror("fopen failed");
 		exit(EXIT_FAILURE);
-    }
-    
-    
-    return value;
+	}
+	
+	
+	return value;
 }
 
-void setGPIO(char* pin, char value) {
-    char* path = getGPIOPath(pin, PATH_SUFFIX_VALUE);
-    
+void GPIO_set(char* pin, char value) {
+	char* path = getGPIOPath(pin, PATH_SUFFIX_VALUE);
+	
 	char strValue = value + '0';
 	char buffer[2] = { strValue, 0 };   
-    writeSafe(path, (char*) buffer);
+	writeSafe(path, (char*) buffer);
 }
 
 
-void exportGPIO(char* pin) {
+void GPIO_export(char* pin) {
 	writeSafe((char*) PATH_EXPORT, pin);
 }
 
-void unexportGPIO(char* pin) {
+void GPIO_unexport(char* pin) {
 	writeSafe((char*)PATH_UNEXPORT, pin);
 }
 
@@ -150,9 +153,9 @@ void *threadFunc(void *arg)
 	int blink_sleep = (1000 * 1000) / BLINK_HERTZ;
 	int value = 1;
 
-	exportGPIO((char*) WRITE_PIN);
+	GPIO_export((char*) WRITE_PIN);
 	GPIO_setDirection((char*) WRITE_PIN, (char*) "out");
-    while (killswitch) {
+	while (killswitch) {
 		//value = (~value) & 1
 		if (value == 1) {
 			value = 0;
@@ -161,49 +164,53 @@ void *threadFunc(void *arg)
 		}
 			
 		if (enableBlinking == 1) {
-			 setGPIO((char*) WRITE_PIN, value);
+			 GPIO_set((char*) WRITE_PIN, value);
 		}
 			  
-        
-        usleep(blink_sleep);
-    }
-    
-    unexportGPIO((char*) WRITE_PIN);
+		
+		usleep(blink_sleep);
+	}
+	
+	GPIO_unexport((char*) WRITE_PIN);
 	return NULL;
 }
 
 void sig_handler(int signo)
 {
 	if (signo == SIGINT){
-		  printf("received SIGINT\n");
+		  printf("received SIGINT, terminating\n");
 		  killswitch = 0;
 	}
 }
 
 
-int main() {
-	 if (signal(SIGINT, sig_handler) == SIG_ERR){
+int main(int argc, char** argv) {
+	printf("RESY Task 1.2 5Hz Blink\n");
+	
+	if (argc == 2 && argv[1] == "-v"){
+		verbose = 1;
+	}
+	
+	if (signal(SIGINT, sig_handler) == SIG_ERR){
 		 printf("Can't catch SIGINT\n");
 	 }
 	
 	pthread_t pth;	
 	pthread_create(&pth, NULL, threadFunc, NULL);
 
-    int oldValue = 1; //default value on rasp pi
-    int numberOfPushes = 0;
-    int val;
 
-	exportGPIO((char*) READ_PIN);
-    while (killswitch) {
-        if ((val = readGPIO((char*) READ_PIN)) > -1) {
-			enableBlinking = val;           
-        }
-        usleep(SLEEPTIME);
-    }
-	unexportGPIO((char*) READ_PIN);
-    
+	GPIO_export((char*) READ_PIN);
+	while (killswitch) {
+		int value;
+		if ((value = readGPIO((char*) READ_PIN)) > -1) {
+			enableBlinking = value;           
+		}
+		usleep(SLEEPTIME);
+	}
+	GPIO_unexport((char*) READ_PIN);
+	
 	//wait for the read thread to cleanup & quit
-    pthread_join(pth, NULL);	    
-    return EXIT_SUCCESS;
+	pthread_join(pth, NULL);	    
+	return EXIT_SUCCESS;
 }
 
