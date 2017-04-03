aa8fe8768c83761da1fcd223bb8a74132e105c46
Henry <blauherr@yahoo.de>
2017-04-03 14:05:08 +0200

    moved v1.1 to the gpio lib

--- v1/switch.c
+++ v1/switch.c
@@ -6,6 +6,9 @@
 //
 //
 
+#include "helper.h"
+#include "gpio.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -17,60 +20,8 @@
 #include <errno.h>
 #include <limits.h>
 
-const int PATHSIZE = 21;
-const char* PATH = "/sys/class/gpio/gpio";
-const char* PATH_SUFFIX = "/value";
-const int PIN = 17;
+const char* READ_PIN = 17;
 const int SLEEPTIME = 100 * 1000; // 1/10 second
-
-
-void enforceMalloc(void* ptr) {
-    if (ptr == NULL) {
-        //If malloc failes, perror failes also. A direct works.
-        char * out = "Malloc failed!\n";
-        write(2, out, sizeof("Malloc failed!\n"));
-        //perror("Malloc failed!");
-        exit(EXIT_FAILURE);
-    }
-}
-
-int readGPIO(int pin) {
-    int value = -1; //default ret
-       
-    //preparing for open
-    int sizeConcat = strlen(PATH) + 1 + strlen(PATH_SUFFIX);
-    
-    if (pin >= 10)
-        sizeConcat++;
-    
-    char* concatPath = (char*) malloc(sizeConcat + 1); //nullbyte
-    enforceMalloc(concatPath);
-    sprintf(concatPath, "%s%d%s", PATH, pin, PATH_SUFFIX);
-
-    //opening and reading Pin
-    FILE *gpio;
-    char buffer[10];
-    
-    gpio = fopen(concatPath, "r");
-    if (gpio != NULL){
-        fread(buffer, 10, 1, gpio);
-
-        value = buffer[0] - '0'; 
-	if (value != 0 && value != 1){
-		printf("Value is unexpected. Buffer: %s\n", buffer);
-		exit(EXIT_FAILURE);
-	}
-
-	fclose(gpio);
-    } else {
-	perror("fopen failed");
-	exit(EXIT_FAILURE);
-    }
-    
-    
-    return value;
-}
-
 
 
 int main() {
@@ -79,8 +30,9 @@ int main() {
     int numberOfPushes = 0;
     int val;
     
+    GPIO_export((char*) READ_PIN);
     while (isRunning) {
-        if ((val = readGPIO(PIN)) > -1) {
+        if ((val = GPIO_read((char*) READ_PIN)) > -1) {
             if (val != oldValue ) {
                 oldValue = val;
                 if (val == 0) {
@@ -93,6 +45,7 @@ int main() {
         
     }
     
+    GPIO_unexport((char*) READ_PIN);
     
     return EXIT_SUCCESS;
 }
