2bc58c99953b76025e7195b58db1bb4376a9816a
Marc Bormeth <marc.bormeth@icloud.com>
2017-04-12 13:33:18 +0200

    rollin

--- v3/engine.c
+++ v3/engine.c
@@ -5,12 +5,16 @@
 #include "helper.h"
 #include "gpio.h"
 #include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+
 
 #define PIN_1 "5"
 #define PIN_2 "6"
 #define PIN_3 "13"
 #define PIN_4 "19"
 
+#define True "42"
 
 enum direction {
     stop = 0,
@@ -33,21 +37,79 @@ void engineSetup(){
     GPIO_setDirection(PIN_4, PIN_OUT);
 }
 
+void engineSetdown(){
+    GPIO_unexport(PIN_1);
+    GPIO_unexport(PIN_2);
+    GPIO_unexport(PIN_3);
+    GPIO_unexport(PIN_4);
+}
+
+void engineStop(){
+    GPIO_set(PIN_1, 0);
+    GPIO_set(PIN_2, 0);
+    GPIO_set(PIN_3, 0);
+    GPIO_set(PIN_4, 0);
+}
+
+void changeMovement(char* pin_1, char* pin_2, int direction){
+    switch (direction) {
+        case forward:
+            GPIO_set(pin_1, 1);
+            GPIO_set(pin_2, 0);
+            break;
+            
+        case stop:
+            GPIO_set(pin_1, 0);
+            GPIO_set(pin_2, 0);
+            break;
+            
+        case reverse:
+            GPIO_set(pin_1, 0);
+            GPIO_set(pin_2, 1);
+            
+            break;
+        default:
+            engineStop();
+            engineSetdown();
+            printf("You can't fly with a pi! Look at the enum direction %d", direction);
+            exit(EXIT_FAILURE);
+    }
+    
+}
 
 void engineDrive(int left, int right){
-    
-    GPIO_set(PIN_1, 0);
-    
+    printf("changeDrive: l %d, r %d\n", left, right);
+    changeMovement(PIN_1, PIN_2, left);
+    changeMovement(PIN_3, PIN_4, right);
+}
 
+
+void sig_handler(int signo)
+{
+    if (signo == SIGINT){
+        printf("received SIGINT, terminating\n");
+        engineStop();
+        engineSetdown();
+        exit(EXIT_SUCCESS);
+    }
 }
 
 
 int main(int argc, const char * argv[]) {
-    
     engineSetup();
-    engineDrive(forward, forward);
-
     
+    if (signal(SIGINT, sig_handler) == SIG_ERR){
+        printf("Can't catch SIGINT\n");
+    }
+    
+    while (True) {
+        int left = genRandom(reverse, forward);
+        int right = genRandom(reverse, forward);
+        engineDrive(left, right);
+        
+        
+        sleep(2);
+    }
     
     
     return EXIT_SUCCESS;
