6ddd56d5c28cbe63bac6c40eddfbda07debf5afb
Marc Bormeth <ma431bor@htwg-konstanz.de>
2017-04-23 00:38:05 +0200

    started using hc-sr04

---
+++ v3/ultrasonic.c
@@ -0,0 +1,50 @@
+/* https://tutorials-raspberrypi.de/entfernung-messen-mit-ultraschallsensor-hc-sr04/ */
+
+#include <stdlib.h>
+#include <unistd.h>
+#include "gpio.h"
+#include "helper.h"
+
+#define PIN_TRIGGER "18"
+#define PIN_ECHO "24"
+#define SONIC_SPEED 34300
+
+#define WAIT_TO_END_TRIGGER_ys 10
+
+
+void ultrasonic_Setup() {
+	GPIO_export(PIN_TRIGGER);
+	GPIO_setDirection(PIN_TRIGGER, PIN_OUT);
+
+	GPIO_export(PIN_ECHO);
+	GPIO_setDirection(PIN_ECHO, PIN_IN);
+}
+
+
+/* TODO:
+ * 		- change from double to int or long (including get_time_ms)
+ *		- define high and low in gpio.h
+ */
+double getDistance() {
+	double startTime, endTime, timeDiff;
+
+	//Trigger measurement
+	GPIO_set(PIN_TRIGGER, '1');
+
+	usleep(WAIT_TO_END_TRIGGER_ys);
+	GPIO_set(PIN_TRIGGER, '0');
+
+	startTime = get_time_ms();
+	endTime = startTime;
+
+	while (GPIO_read(PIN_ECHO) == 1) {	 //Is this too fast for gpio?
+		startTime = get_time_ms();
+	}
+	while (GPIO_read(PIN_ECHO) == 0) {
+		endTime = get_time_ms();
+	}
+
+	timeDiff = endTime - startTime;
+
+	return (timeDiff * SONIC_SPEED) / 2;
+}
