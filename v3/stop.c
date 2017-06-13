cdad7b8c317cf4d8f31ddf5e75ea6874f2685009
GoneUp <henstrob@htwg-konstanz.de>
2017-06-13 13:32:27 +0000

    -.-

---
+++ v3/stop.c
@@ -0,0 +1,14 @@
+//test stop file
+#include "common.h"
+#include "engine.h"
+
+//dummy vars
+volatile engineMode engineCtrl; //see common.h
+volatile int shouldRun = 1;     //see common.h
+cpu_set_t cpuset_logic;     //see common.h
+cpu_set_t cpuset_sensors;   //see common.h
+cpu_set_t cpuset_engine;    //see common.h
+
+int main(int argc, char *argv[]) {	
+    engineSetdown();
+}
